cefd9e4af3ffe4144c0e6107019c7f47dcab7b72
Ian Pilipski <ian@pilipski.com>
2020-10-28 01:21:32 -0400

    updated the version and dependency on the xcode plugin

--- iptech-unity-plugin/src/main/groovy/com/iptech/gradle/unity/internal/buildsteps/InstallProvisioningProfiles.groovy
+++ iptech-unity-plugin/src/main/groovy/com/iptech/gradle/unity/internal/buildsteps/InstallProvisioningProfiles.groovy
@@ -3,6 +3,7 @@ package com.iptech.gradle.unity.internal.buildsteps
 import com.iptech.gradle.unity.api.BuildConfig
 import com.iptech.gradle.unity.api.BuildStep
 import org.gradle.api.Task
+import com.iptech.gradle.xcode.tasks.InstallProvisioningProfiles
 
 class InstallProvisioningProfiles implements BuildStep {
 
@@ -17,7 +18,7 @@ class InstallProvisioningProfiles implements BuildStep {
     }
 
     Task installProvisioningProfiles(String taskPrefex, BuildConfig buildConfig, Closure config ) {
-        return buildConfig.unity.project.tasks.create(taskPrefex, com.iptech.gradle.xcode.tasks.InstallProvisioningProfiles) {
+        return buildConfig.unity.project.tasks.create(taskPrefex, InstallProvisioningProfiles) {
             provisioningProfiles = buildConfig.unity.project.fileTree( 'profiles') { include '*.mobileprovision' }
         }.configure(config?:{})
     }
